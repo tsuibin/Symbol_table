8b3e316cea47d9247c1e7e632eb9091170912200
Tsuibin <tsuibin@o2ee.com>
2015-11-27 18:28:27 +0800

    update

---
+++ SymbolTable/main.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void funcName()
+{
+    printf("aaaaaa\n");
+}
+
+
+
+class Tsuibin{
+    public:
+    Tsuibin(){}
+
+    void myFunction(){
+        printf("111111111111111111\n");
+    }
+
+
+};
+
+int main(int argc, char *argv[])
+{
+
+    Tsuibin test;
+    test.myFunction();
+}
